1ca73793fb193e1c9e855669d28878619079ec0e
maximgorbachiov <maksimka.gorbachev.95@gmail.com>
2023-08-01 22:24:41 +0200

    moved app secrets to Azure Key vault

--- Transaction Service/CostManager.TransactionService.FuncApp/CostManager.TransactionService.FuncApp/Extensions/CosmosDbExtensions.cs
+++ Transaction Service/CostManager.TransactionService.FuncApp/CostManager.TransactionService.FuncApp/Extensions/CosmosDbExtensions.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CostManager.TransactionService.FuncApp.Extensions
 {
@@ -9,9 +13,25 @@ namespace CostManager.TransactionService.FuncApp.Extensions
         {
             services.AddSingleton(s =>
             {
+                SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                    }
+                };
+
+                var client = new SecretClient(new Uri("https://secrets-storage.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+                KeyVaultSecret endpoint = client.GetSecret("transactions-cosmosdb-endpoint");
+                KeyVaultSecret authToken = client.GetSecret("transactions-cosmosdb-authtoken");
+
                 return new CosmosClient(
-                    accountEndpoint: "https://transactions-storage.documents.azure.com:443",
-                    authKeyOrResourceToken: "kR5lrifRmEr29PAXtlh7N2rViTJC2FZYYTEC79EvosAXsljUDuBbLlPgQidjr6OT69datJsDZf6nACDbljDnLA=="
+                    accountEndpoint: endpoint.Value,
+                    authKeyOrResourceToken: authToken.Value
                 );
             });
         }
