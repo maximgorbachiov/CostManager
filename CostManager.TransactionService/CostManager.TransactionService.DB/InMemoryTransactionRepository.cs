4ec5c50e11272351147f9d5508731c635c709f77
maximgorbachiov <maksimka.gorbachev.95@gmail.com>
2025-02-27 21:44:39 +0100

    update for cosmos db

--- CostManager.TransactionService/CostManager.TransactionService.DB/InMemoryTransactionRepository.cs
+++ CostManager.TransactionService/CostManager.TransactionService.DB/InMemoryTransactionRepository.cs
@@ -11,7 +11,7 @@ namespace CostManager.TransactionService.DB
         {
             var transaction = new Transaction
             {
-                id = Guid.NewGuid().ToString(),
+                TransactionId = Guid.NewGuid().ToString(),
                 Sum = addTransaction.Sum,
                 PlaceOfTransaction = addTransaction.PlaceOfTransaction,
                 Description = addTransaction.Description,
@@ -21,14 +21,14 @@ namespace CostManager.TransactionService.DB
 
             _transactions.Add(transaction);
 
-            return await Task.FromResult(transaction.id);
+            return await Task.FromResult(transaction.TransactionId);
         }
 
         public async Task<List<TransactionModel>> GetTransactionsListAsync()
         {
             var result = _transactions.Select(t => new TransactionModel
             {
-                TransactionId = t.id,
+                TransactionId = t.TransactionId,
                 Sum = t.Sum,
                 PlaceOfTransaction = t.PlaceOfTransaction,
                 Description = t.Description,
@@ -41,7 +41,7 @@ namespace CostManager.TransactionService.DB
 
         public async Task<bool> RemoveTransactionAsync(string userId, string transactionId)
         {
-            var transaction = _transactions.FirstOrDefault(t => t.id == transactionId);
+            var transaction = _transactions.FirstOrDefault(t => t.TransactionId == transactionId);
             
             bool result = transaction != null;            
 
